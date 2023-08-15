eafa0d7752fca7609f5394a508c13cae268443e7
paper <faition2@gmail.com>
2023-08-15 23:30:36 +0800

    feat: update and delete data use mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -60,25 +60,34 @@ const getTour = async (req, res) => {
   }
 }
 
-const updateTour = (req, res) => {
-  // if (req.params.id > tours.length) {
-  //   return res.status(404).json({
-  //     status: 'fail', message: 'Invalid ID'
-  //   })
-  // }
-  res.status(200).json({ status: 'success', data: { tour: '<Updated tour here...>' } })
+const updateTour = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tour = await Tour.findByIdAndUpdate(
+      id,
+      req.body, // 要更新的資料
+      {
+        new: true, // new 會回傳新的資料
+        runValidators: true, // 在 schema 設的 validate 會再跑一次
+      }
+    );
+    res.status(200).json({ status: 'success', data: { tour } })
+  } catch(err) {
+    res.status(400).json({ status: 'fail', message: err });
+  }
 }
 
-const deleteTour = (req, res) => {
-  // if (req.params.id > tours.length) {
-  //   return res.status(404).json({
-  //     status: 'fail', message: 'Invalid ID'
-  //   })
-  // }
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+const deleteTour = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Tour.findByIdAndDelete(id);
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch(err) {
+    res.status(400).json({ status: 'fail', message: err });
+  }
 }
 
 module.exports = {
