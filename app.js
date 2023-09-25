f9d6e2e9a11de668404247042cd2e492a4896d13
paper <faition2@gmail.com>
2023-09-25 16:34:55 +0800

    handle unhandle routes

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-  console.log('middleware');
-  next();
-})
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 })
@@ -28,5 +23,13 @@ app.use((req, res, next) => {
 // tourRouter/userRouter 也是一個 middleware
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter);
+
+// handle unhandle routes
+app.all('*', (req, res) => {
+  res.status(400).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl}.`
+  })
+});
 
 module.exports = app;
